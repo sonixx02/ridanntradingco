0572f7412079fd3d3fba1633e1437453c5b7cb36
Soni Darshan <soni.darshan0209@gmail.com>
2025-08-12 12:45:54 +0530

    changed viteconfig

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'build', 
-    chunkSizeWarningLimit: 2000, 
+    outDir: 'dist',  
+    chunkSizeWarningLimit: 2000,
   },
   plugins: [
-    tsconfigPaths(), 
-    react(),        
+    tsconfigPaths(),
+    react(),
   ],
 });
+
